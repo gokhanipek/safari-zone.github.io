ff103c6bc17f03ae22184deccfda11f12e83632d
unknown <gokhan.ipek@etiya.com>
2019-07-22 04:03:35 +0300

    'build'

---
+++ build/precache-manifest.33a4cc7858d7d3b1dd3b5bf0f2076627.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8029a9df0d0c97acaebcea0c1ff4b487",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f91f9c33d3cb69c87539",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "b9a1356e3ad6c3c5a91e",
+    "url": "/static/css/main.ed42a9e6.chunk.css"
+  },
+  {
+    "revision": "f91f9c33d3cb69c87539",
+    "url": "/static/js/2.a715e718.chunk.js"
+  },
+  {
+    "revision": "b9a1356e3ad6c3c5a91e",
+    "url": "/static/js/main.90615159.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
